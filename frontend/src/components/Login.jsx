30a02f30bfeff8ae52ddb64fed3f5d1a846a28eb
Aayush Patel <rudaniaayush1@gmail.com>
2023-03-17 17:24:02 +0530

    bg image issue resolved

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,12 +3,9 @@ import LoginImg from "../assets/loginImg2.jpeg"
 import { NavLink } from "react-router-dom";
 export default function Login() {
 	return (
-		<div className="relative h-screen w-full bg-zinc-600">
-		<img
-		  src={LoginImg}
-		  alt=""
-		  className="absolute h-full w-full object-cover mix-blend-overlay"
-		/>  
+		<div className={`relative h-screen w-full bg-zinc-600`} style={{
+				backgroundImage: `url(${LoginImg})`,
+			}}>
 		<div className="flex flex-col justify-center items-center h-full">
 		  <div action="" className="max-w-[480px] rounded-xl shadow-lg shadow-black p-8 w-full mx-auto bg-white">
 			<h2 className="font-bold text-4xl mb-8 text-center py-4">Welcome Back!</h2>
